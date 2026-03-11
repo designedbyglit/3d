992b124309db0dca69217533468b68a84a0e8b8b
designedbyglit <afbgraphics45230@gmail.com>
2026-03-11 21:22:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ geometry.center();
 logo.scale.set(0.7,0.7,0.7);
 
 // POSITION
-logo.position.set(0,12,10);
+logo.position.set(0,120,100);
 
 scene.add(logo);
   
@@ -166,4 +166,5 @@ link.click();
 
 
 }
+
 
